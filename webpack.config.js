74242f820094d7e83fd8284e333b44a2872cc749
Randy Andriamaro <randy.andriamaro@outlook.fr>
2019-04-19 00:03:56 +0200

    add babel, webpack dependencies

---
+++ webpack.config.js
@@ -0,0 +1,69 @@
+"use strict"
+
+const path = require("path")
+const { VueLoaderPlugin } = require("vue-loader")
+
+module.exports = {
+    target: "web",
+
+    entry: {
+        index: "./src/index.js",
+    },
+
+    output: {
+        filename: "[name].js",
+        path: path.resolve(__dirname, "dist")
+    },
+
+    mode: "development",
+
+    module: {
+        rules: [
+            {   // Vue
+                test: /\.vue$/,
+                use: "vue-loader"
+            },
+            {   // Babel
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: "babel-loader",
+            },
+            {   // Styles
+                test: /\.(css|scss)$/,
+                use: [
+                    "style-loader",
+                    "css-loader",
+                    "sass-loader"
+                ]
+            },
+            {   // Fonts
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                use: [
+                    {
+                        loader: "file-loader",
+                        options: {
+                            name: "[name].[ext]",
+                            outputPath: "fonts"
+                        }
+                    }
+                ]
+            },
+            {   // Images
+                test: /\.(jpg|jpeg|png|svg)$/,
+                use: [
+                    {
+                        loader: "file-loader",
+                        options: {
+                            name: "[name].[ext]",
+                            outputPath: "images"
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    
+    plugins: [
+        new VueLoaderPlugin()
+    ],
+}
