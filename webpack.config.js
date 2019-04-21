62d836fee08fe9c0ffadf1d4b39c46a4014fb9d3
Randy Andriamaro <randy.andriamaro@outlook.fr>
2019-04-21 23:39:13 +0200

    added babel polyfill

--- webpack.config.js
+++ webpack.config.js
@@ -5,70 +5,70 @@ const HtmlWebpackPlugin = require("html-webpack-plugin")
 const { VueLoaderPlugin } = require("vue-loader")
 
 module.exports = {
-    target: "web",
+	target: "web",
 
-    entry: {
-        index: "./src/index.js",
-    },
+	entry: {
+		index: "./src/index.js",
+	},
 
-    output: {
-        filename: "[name].js",
-        path: path.resolve(__dirname, "dist")
-    },
+	output: {
+		filename: "[name].js",
+		path: path.resolve(__dirname, "dist"),
+	},
 
-    mode: "development",
+	mode: "development",
 
-    module: {
-        rules: [
-            {   // Vue
-                test: /\.vue$/,
-                use: "vue-loader"
-            },
-            {   // Babel
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loader: "babel-loader",
-            },
-            {   // Styles
-                test: /\.(css|scss)$/,
-                use: [
-                    "style-loader",
-                    "css-loader",
-                    "sass-loader"
-                ]
-            },
-            {   // Fonts
-                test: /\.(woff|woff2|eot|ttf|otf)$/,
-                use: [
-                    {
-                        loader: "file-loader",
-                        options: {
-                            name: "[name].[ext]",
-                            outputPath: "fonts"
-                        }
-                    }
-                ]
-            },
-            {   // Images
-                test: /\.(jpg|jpeg|png|svg)$/,
-                use: [
-                    {
-                        loader: "file-loader",
-                        options: {
-                            name: "[name].[ext]",
-                            outputPath: "images"
-                        }
-                    }
-                ]
-            }
-        ]
-    },
-    
-    plugins: [
-        new VueLoaderPlugin(),
-        new HtmlWebpackPlugin({
-            template: "src/index.html",
-            favicon: "./favicon.ico"
-        })
-    ],
+	module: {
+		rules: [
+			{ // Vue
+				test: /\.vue$/,
+				use: "vue-loader",
+			},
+			{ // Babel
+				test: /\.js$/,
+				exclude: /node_modules/,
+				loader: "babel-loader",
+			},
+			{ // Styles
+				test: /\.(css|scss)$/,
+				use: [
+					"style-loader",
+					"css-loader",
+					"sass-loader",
+				],
+			},
+			{ // Fonts
+				test: /\.(woff|woff2|eot|ttf|otf)$/,
+				use: [
+					{
+						loader: "file-loader",
+						options: {
+							name: "[name].[ext]",
+							outputPath: "fonts",
+						},
+					},
+				],
+			},
+			{ // Images
+				test: /\.(jpg|jpeg|png|svg)$/,
+				use: [
+					{
+						loader: "file-loader",
+						options: {
+							name: "[name].[ext]",
+							outputPath: "images",
+						},
+					},
+				],
+			},
+		],
+	},
+
+	plugins: [
+		new VueLoaderPlugin(),
+		new HtmlWebpackPlugin({
+			template: "src/index.html",
+			favicon: "./favicon.ico",
+		}),
+	],
 }
